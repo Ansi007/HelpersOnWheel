14bb245309027eec22300c1ea060f2a6423e88bc
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-01 12:08:36 +0500

    Nav Bar created on Home Page

---
+++ HOW/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HOW.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
