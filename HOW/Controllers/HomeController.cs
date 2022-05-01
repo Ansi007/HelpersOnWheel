9bfc16c98ac304adaf3ede7803b4c6effec3736b
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-01 11:25:18 +0500

    initial commit

---
+++ HOW/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿//using HOW.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HOW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
