623490e31bea7a8ec44ae73e0778efbe9bc74f46
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-25 17:02:14 +0500

    Edit View Added

---
+++ HOW/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HOW.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Timeline()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
